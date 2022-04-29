0b2151ee14b98df15401e6e38f516fd665e5cd2a
aliamin <aliahmad.othman@techvista.com>
2022-04-29 14:01:28 +0400

    add-documentaion-to-Base-Protocols

--- CLEAN Architecture/CLEANBase.swift
+++ CLEAN Architecture/CLEANBase.swift
@@ -1,15 +1,33 @@
 //
-//  Base.swift
-//  NY-TimesNews
+//  CLEANBase.swift
 //
 //  Created by Ali Amin on 26/04/2022.
 //
+//  CLEANBase is a set of protocols that helps creating an architecture Based on the Clean Architecture
+/// In this architecture all logic is put into ViewModel. that in turn will split different types of logic into other components.
+/// Presentation is the responsibility of ViewController and all other UI elements. But it should always remain dumb. That it doean't perm any logic. Not even presentation logic
+/// Logic can be split into three different types. Presentation logic, Domain/Business logic and Data retrieval logic.
+/// Networking logic is in a separate layer
+
 
 
 import Foundation
 import UIKit
 
+
+
 // MARK: Coordinator
+/// Coordinator: is responsible of the coordination of flow between different scenes, including all navigation/routing logic.
+/// It contains the presentation and logic layers (ViewController and ViewModel respectively).
+/// Interaction between coordinaores should happen to perfoem the navigation/flow from scene to another
+/// - InoutType: Is the dependencies of the coordinator. For example MyCoordinator needs the current NavigationController and the value of language app set to. So the InputType typealias will be a struct that contains both UINavigationController and String peroperties.
+/// ```
+/// struct MyCoordinatorInput {
+///     let navigationController: UINavigationController
+///     let language: String
+/// }
+/// ```
+/// - ActionsType: Actions are the channel via which coordinator can communicate with caller objet (mainly another coordinator). It can be a closure
 protocol Coordinator {
     associatedtype InputType
     associatedtype ActionsType
