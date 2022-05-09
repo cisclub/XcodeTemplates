50462ebfdfb383e5ce0339c5acfd165cd5c9c43a
aliamin <aliahmad.othman@techvista.com>
2022-05-09 16:48:10 +0400

    remove viewController from viewModel init

--- CLEAN Architecture/CLEAN Templates/CLEAN.xctemplate/___VARIABLE_sceneName___Scene/Domain/___VARIABLE_useCaseName___ViewModel.swift
+++ CLEAN Architecture/CLEAN Templates/CLEAN.xctemplate/___VARIABLE_sceneName___Scene/Domain/___VARIABLE_useCaseName___ViewModel.swift
@@ -9,9 +9,15 @@ struct ___VARIABLE_useCaseName___ViewModel: ViewModel {
     typealias ActionsType = ___VARIABLE_useCaseName___ViewModelActions
     
     
-    let viewController: UIViewController?
+    let viewController: UIViewController? = nil
     let useCases: UseCasesType
     let actions: ActionsType
+    
+    
+    init(useCases: UseCasesType, actions: ActionsType) {
+        self.useCases = useCases
+        self.actions = actions
+    }
 }
 
 
